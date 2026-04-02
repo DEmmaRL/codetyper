0afa7a5946babee56895a59b7f153b6831621ed4
DEmmaRL <demmarlmiau@gmail.com>
2026-04-01 18:20:42 -0600

    add initial short-test template with basic structure

---
+++ templates/short-test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+}
+
+int main() {
+    _
+    lli q = 1 ;
+   // cin>>q;
+    
+    fore(i,0,q){
+
+      solve();
+    }
+    
+    return 0;
+}
